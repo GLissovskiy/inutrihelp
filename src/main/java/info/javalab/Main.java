55d561e8092fde8df41a120e1d8b7314286c0971
glissovskiy <glissovskiy@gmail.com>
2020-01-09 21:41:53 -0800

    welcome to self nutritionist

--- src/main/java/info/javalab/Main.java
+++ src/main/java/info/javalab/Main.java
@@ -7,6 +7,6 @@ package info.javalab;
  */
 public class Main {
     public static void main(String [] args){
-        System.out.println("Welcome.");
+        System.out.println("Welcome to self nutritionist.");
     }
 }
