4840dcb1055ee852d724fc08af2eeb5ff16ee219
LeGion <glissovskiy@gmail.com>
2020-02-08 19:26:02 -0800

    Test from SpringMain

--- src/main/java/info/javalab/inutrihelp/SpringMain.java
+++ src/main/java/info/javalab/inutrihelp/SpringMain.java
@@ -3,11 +3,17 @@ package info.javalab.inutrihelp;
 import info.javalab.inutrihelp.model.Role;
 import info.javalab.inutrihelp.model.User;
 import info.javalab.inutrihelp.repository.UserRepository;
+import info.javalab.inutrihelp.to.MealTo;
+import info.javalab.inutrihelp.web.meal.MealRestController;
 import info.javalab.inutrihelp.web.user.AdminRestController;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
 import java.util.Arrays;
+import java.util.List;
 
 public class SpringMain {
     public static void main(String[] args) {
@@ -16,6 +22,14 @@ public class SpringMain {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+            System.out.println();
+
+            MealRestController mealController = appCtx.getBean(MealRestController.class);
+            List<MealTo> filteredMealsWithExcess =
+                    mealController.getBetween(
+                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
+            filteredMealsWithExcess.forEach(System.out::println);
         }
     }
 }
