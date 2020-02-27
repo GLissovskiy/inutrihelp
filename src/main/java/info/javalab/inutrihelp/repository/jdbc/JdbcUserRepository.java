d5c0912a2eaee73b5b1b5a363878fcc031f20aa8
LeGion <glissovskiy@gmail.com>
2020-02-26 21:46:26 -0800

    Remarks

--- src/main/java/info/javalab/inutrihelp/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/info/javalab/inutrihelp/repository/jdbc/JdbcUserRepository.java
@@ -9,6 +9,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
