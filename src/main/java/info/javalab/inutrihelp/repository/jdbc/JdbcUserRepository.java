454199f48472ba38f1c4811d1f8599affad37b43
LeGion <glissovskiy@gmail.com>
2020-03-25 21:33:38 -0700

    Fixed bugs

--- src/main/java/info/javalab/inutrihelp/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/info/javalab/inutrihelp/repository/jdbc/JdbcUserRepository.java
@@ -29,6 +29,7 @@ public class JdbcUserRepository implements UserRepository {
         this.insertUser = new SimpleJdbcInsert(jdbcTemplate)
                 .withTableName("users")
                 .usingGeneratedKeyColumns("id");
+
         this.jdbcTemplate = jdbcTemplate;
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
 
